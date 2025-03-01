bfe748a1c7be8300754f253ff454eab5f9c4415c
18alantom <2.alan.tom@gmail.com>
2025-03-01 19:32:40 +0530

    add pending albums
    - add projects to index
    - pin some posts and projects
    - minor styling changes

--- index.js
+++ index.js
@@ -1,4 +1,135 @@
-const posts = [
+const projects = [
+  {
+    title: 'synth',
+    date: '2021-02-14',
+    description: 'An attempt at making a synth using Python',
+    location: 'GitHub',
+    link: 'https://github.com/18alantom/synth',
+  },
+  {
+    title: 'zettel.vim',
+    date: '2021-09-19',
+    description: 'A Vim plugin to hyperlink files',
+    location: 'GitHub',
+    link: 'https://github.com/18alantom/zettel.vim',
+    pinned: true,
+  },
+  {
+    title: 'Strawberry',
+    date: '2023-03-27',
+    description: 'Zero-dependency, build-free framework for the artisanal web',
+    location: 'Strawberry',
+    link: 'https://strawberry.quest/',
+    pinned: true,
+  },
+  {
+    title: 'asyncpy-server-benchmarks',
+    date: '2023-11-24T13:52:50Z',
+    description:
+      'Synthetic benchmarks of Python WSGI and ASGI apps and servers',
+    location: 'GitHub',
+    link: 'https://github.com/18alantom/asyncpy-server-benchmarks',
+  },
+  {
+    title: 'bm_exp',
+    date: '2024-08-09T08:30:16Z',
+    description:
+      'An experiment in Go to see if multiple Frappe apps can be installed concurrently',
+    location: 'GitHub',
+    link: 'https://github.com/18alantom/bm_exp',
+  },
+  {
+    title: 'Frappe Books',
+    date: '2021-09-02T18:29:51Z',
+    description:
+      'Free Desktop book-keeping software for small-businesses and freelancers',
+    location: 'Frappe [work]',
+    link: 'https://frappe.io/books',
+    pinned: true,
+  },
+  {
+    title: 'concurrent_inference',
+    date: '2021-01-13T07:28:55Z',
+    description:
+      'An example of how to use the multiprocessing package along with PyTorch',
+    location: 'GitHub',
+    link: 'https://github.com/18alantom/concurrent_inference',
+  },
+  {
+    title: 'CoveragePathPlanning',
+    date: '2020-07-19T18:52:03Z',
+    description: 'Code pertaining to coverage path planning and area division',
+    location: 'GitHub',
+    link: 'https://github.com/18alantom/CoveragePathPlanning',
+  },
+  {
+    title: 'fetzen',
+    date: '2019-07-28T14:10:11Z',
+    description: 'A workout tracking web app.',
+    location: 'GitHub',
+    link: 'https://github.com/18alantom/fetzen',
+  },
+  {
+    title: 'fex',
+    date: '2024-03-25T06:51:59Z',
+    description:
+      'A command-line file explorer in Zig that prioritizes quick navigation',
+    location: 'GitHub',
+    link: 'https://github.com/18alantom/fex',
+    pinned: true,
+  },
+  {
+    title: 'fitloop',
+    date: '2020-05-06T00:14:49Z',
+    description: '\u27b0 fitloop trains Pytorch models',
+    location: 'GitHub',
+    link: 'https://github.com/18alantom/fitloop',
+  },
+].map((p) => ({ ...p, type: 'project' }));
+
+const albums = [
+  {
+    title: 'Mold',
+    date: '2025-02-10',
+    description: 'I wanted to test for mold, but I ran out of petri dishes',
+    location: '/photos',
+    link: '/photos.html?album=mold',
+  },
+  {
+    title: 'Colaba',
+    date: '2025-02-02',
+    description: 'Black and white, or blue and orange shots from Colaba',
+    location: '/photos',
+    link: '/photos.html?album=colaba',
+  },
+  {
+    title: 'Creature Portraits',
+    date: '2025-01-22',
+    description: 'Black and white portraits of creatures (non human)',
+    location: '/photos',
+    link: '/photos.html?album=creature_portraits',
+  },
+  {
+    title: 'Untitled II',
+    date: '2025-01-17',
+    description: 'Two photos from a walk near my house',
+    location: '/photos',
+    link: '/photos.html?album=untitled_II',
+  },
+  {
+    title: 'Near The Mangroves',
+    date: '2024-12-31',
+    description: 'High tide explains some of the strangeness',
+    location: '/photos',
+    link: '/photos.html?album=near_the_mangroves',
+  },
+  {
+    title: 'Seawoods',
+    date: '2024-12-28',
+    description: 'Photos from walks around Seawoods',
+    location: '/photos',
+    link: '/photos.html?album=seawoods',
+  },
   {
     title: 'Untitled I',
     date: '2024-11-23',
@@ -21,6 +152,9 @@ const posts = [
     location: '/photos',
     link: '/photos.html?album=ladakh',
   },
+].map((p) => ({ ...p, type: 'album' }));
+
+const posts = [
   {
     title: 'CODE_READABILITY.md',
     date: '2024-07-05',
@@ -36,6 +170,7 @@ const posts = [
     description:
       'Or how hard is it to build a frontend framework with only Web APIs and no dependencies?',
     link: '/posts/how-hard-is-it-to-build-a-frontend-framework.html',
+    pinned: true,
   },
   {
     title: 'Misanthropy Thwarted',
@@ -81,6 +216,7 @@ const posts = [
       'I had to rewrite the charts in Frappe Books. This seemed like the only option. Well almost.',
     location: 'Frappe Books Tech Blog',
     link: '/frappebooks_tech/refactoring_charts',
+    pinned: true,
   },
   {
     title: 'Mumbai University, Everyone Cheated',
@@ -89,6 +225,7 @@ const posts = [
     date: '2021-06-21',
     location: 'Medium',
     link: 'https://18alan.medium.com/mumbai-university-everyone-cheated-83320b8c351a',
+    pinned: true,
   },
   {
     title: 'Making A Synth With Python — Controllers',
@@ -105,6 +242,7 @@ const posts = [
     date: '2021-02-22',
     location: 'Medium',
     link: 'https://python.plainenglish.io/build-your-own-python-synthesizer-part-2-66396f6dad81',
+    pinned: true,
   },
   {
     title: 'Making A Synth With Python — Oscillators',
@@ -114,38 +252,35 @@ const posts = [
     location: 'Medium',
     link: 'https://python.plainenglish.io/making-a-synth-with-python-oscillators-2cb8e68e9c3b',
   },
-]
+].map((p) => ({ ...p, type: 'post' }));
+
+const index = [...posts, ...albums, ...projects]
   .sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf())
+  .sort((a, b) => Number(b.pinned ?? false) - Number(a.pinned ?? false))
   .map((p) => formatPost(p));
 
-function formatPost(post) {
-  post.type ??= 'post';
-  if (post.location !== undefined && !post.location.startsWith('/')) {
-    post.type = 'external';
-  } else if (post.location?.startsWith('/photos')) {
-    post.type = 'album';
-  }
+function formatPost(item) {
+  item.date = formatDate(item.date);
+  item.description = item.description ?? '';
+  item.location ??= '/posts';
 
-  post.date = formatDate(post.date);
-  post.description = post.description ?? '';
-  post.location ??= '/posts';
-
-  if (!post.link) {
+  if (!item.link) {
     // 'Post Zero, Why?' -> 'post-zero-why'
-    const slug = post.title
+    const slug = item.title
       .toLowerCase()
       .replace('-', '')
       .replace(/\s+/g, '-')
       .replace(/[^-a-z]+/g, '');
 
-    post.link = `posts/${slug}.html`;
+    item.link = `posts/${slug}.html`;
   }
 
-  if (post.link.startsWith('/') && window.location.pathname) {
-    post.link = window.location.pathname + post.link.slice(1);
+  if (item.link.startsWith('/') && window.location.pathname) {
+    item.link = window.location.pathname + item.link.slice(1);
   }
 
-  return post;
+  item.isExternal = item.link?.startsWith('http') ?? false;
+  return item;
 }
 
 function formatDate(date) {
@@ -156,30 +291,32 @@ function formatDate(date) {
   return `${month} ${day}, ${year}`;
 }
 
-function populatePosts(filters) {
+function populatePosts(filter) {
   const postsSection = document.getElementById('posts');
   const domParser = new DOMParser();
 
   postsSection.innerHTML = '';
-  for (const post of posts) {
-    if (post.type === 'post' && !filters.post) continue;
-    if (post.type === 'external' && !filters.external) continue;
-    if (post.type === 'album' && !filters.album) continue;
+  for (const item of index) {
+    if (item.type !== filter) continue;
 
     const post_str = `
-      <a href="${post.link}">
-        <h2 class="title">${post.title}</h2>
+      <a href="${item.link}">
+        <h2 class="title">${item.title}</h2>
         <div class="date-location">
-          <time>${post.date}</time> · <p>${post.location}</p>
+          <time>${item.date}</time> · <p>${item.location}</p>
         </div>
-        <p class="description">${post.description}</p>
+        <p class="description">${item.description}</p>
       </a>
     `.trim();
 
     const element = domParser.parseFromString(post_str, 'text/html').body
       .children[0];
 
-    if (post.type === 'external') {
+    if (item.pinned) {
+      element.classList.add('pinned');
+    }
+
+    if (item.isExternal) {
       element.target = '_blank';
       element.relList = ['noreferrer', 'noopener'];
     }
@@ -188,41 +325,41 @@ function populatePosts(filters) {
 }
 
 function init() {
-  let filters = JSON.parse(localStorage.getItem('filters') ?? '{}');
+  const params = new URLSearchParams(window.location.search);
+  let filter = params.get('filter') ?? 'post'; // "post" | "album" | "project"
 
-  function filterFix() {
-    if (filters.album || filters.post || filters.external) return;
-    filters.post = true;
-    setStyle(document.querySelector('[data-filter="post"]'));
+  function updateFilter() {
+    if (!['post', 'album', 'project'].includes(filter)) filter = 'post';
+    params.set('filter', filter);
+    if (filter === 'post') params.delete('filter');
+
+    let url = window.location.pathname;
+    if (url.endsWith('/') && params.toString()) url = url.slice(0, -1);
+    if (params.toString()) url += '?' + params;
+
+    window.history.replaceState({}, '', url);
   }
 
-  function setStyle(button) {
-    const value = filters[button.dataset.filter];
-    button.dataset.toggled = value;
+  updateFilter();
+  populatePosts(filter);
+
+  const buttons = [...document.querySelectorAll('[data-filter]')].filter(
+    (b) => b instanceof HTMLButtonElement
+  );
+
+  function setStyle() {
+    buttons.forEach((b) => (b.dataset.toggled = b.dataset.filter === filter));
   }
 
-  filters.post ??= true;
-  filters.album ??= false;
-  filters.external ??= true;
-
-  filterFix();
-  populatePosts(filters);
-
-  const filterDiv = document.getElementById('filters');
-  for (const button of [...filterDiv.children]) {
-    if (!(button instanceof HTMLButtonElement)) continue;
-    setStyle(button);
-
-    button.addEventListener('click', (e) => {
-      filters[button.dataset.filter] = !filters[button.dataset.filter];
-
-      setStyle(button);
-      filterFix();
-      populatePosts(filters);
-
-      localStorage.setItem('filters', JSON.stringify(filters));
-    });
-  }
+  setStyle();
+  buttons.forEach((b) =>
+    b.addEventListener('click', (e) => {
+      filter = b.dataset.filter;
+      setStyle();
+      populatePosts(filter);
+      updateFilter();
+    })
+  );
 }
 
 init();
